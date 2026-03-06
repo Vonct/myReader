c1641cf030e669df436743e98f516b87b370afdf
vonct <1574336755@qq.com>
2026-03-06 20:13:48 +0800

    feat: set basePath to /myreader and fix dev origin config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    allowedDevOrigins: ["localhost:3000", "*.loca.lt"],
-  },
+  basePath: "/myreader",
+  allowedDevOrigins: ["localhost:3000", "*.loca.lt"],
 };
 
 export default nextConfig;
